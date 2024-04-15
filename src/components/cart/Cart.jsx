8d602477c59a4044556548243ea2d750ae88eb14
mattia <sharp798@yahoo.com>
2024-04-15 18:17:37 +0200

    versione 23

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,38 +1,30 @@
+import { lazy, Suspense } from "react";
+import { useSelector } from "react-redux";
+import { calculateCartTotal } from "../../functions/calculateCartTotal";
+const CartItem = lazy(() => import('@components/cart/CartItem'));
+const Button = lazy(() => import('@components/button/Button'));
+
 const Cart = () => {
+  const cart = useSelector((state) => state.cart);
+  const cartTotal = calculateCartTotal(cart);
+
   return (
     <>
-      <div className="bg-white rounded-md shadow lg:h-screen lg:sticky lg:top-0">
+      <div className="bg-white rounded-md shadow">
         <div className="relative h-full">
-          <div className="p-10 lg:overflow-auto lg:h-[calc(100vh)]">
-            <h2 className="text-2xl">Riepilogo</h2>
-            <div className="mt-10 space-y-6">
-              <div className="grid items-start gap-6 sm:grid-cols-2">
-                <div className="px-4 rounded-md py-7 shrink-0 bg-gray-50">
-                  <img
-                    src=""
-                    className="object-contain w-full"
-                    alt=".."
-                  />
-                </div>
-                <div>
-                  <h3 className="text-base">Pizza Rossa</h3>
-                  <ul className="mt-2 space-y-2 text-xs">
-                    <li className="flex flex-wrap gap-4">
-                      Quantity <span className="ml-auto">2</span>
-                    </li>
-                    <li className="flex flex-wrap gap-4">
-                      Total Price <span className="ml-auto">$40</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+          <div className="p-6 lg:overflow-auto lg:h-[calc(100vh)] pb-24">
+            <h2 className="text-2xl mb-5">Riepilogo</h2>
+            <Suspense fallback={null}>
+              <CartItem />
+            </Suspense>
+          </div>
+          <Suspense fallback={null}>
+            <div className="absolute bottom-0 left-0 w-full p-4 bg-gray-100 border-t-2">
+              <h4 className="text-base flex justify-between">
+                Totale ordine <span className="pr-2.5">{cartTotal}</span>
+              </h4>
             </div>
-          </div>
-          <div className="absolute bottom-0 left-0 w-full p-4 bg-gray-100 border-t-2">
-            <h4 className="flex flex-wrap gap-4 text-base">
-              Total <span className="ml-auto">$84.00</span>
-            </h4>
-          </div>
+          </Suspense>
         </div>
       </div>
     </>
--- src/components/cart/CartUserProfile.jsx
+++ src/components/cart/CartUserProfile.jsx
@@ -3,117 +3,108 @@ import { AppContext } from '@context/AppContext';
 import { Formik, Form, Field } from 'formik';
 import useFieldsPopulated from '@hooks//UseFieldPopulated';
 import { useMediaQuery } from 'react-responsive';
-
-// icons
-import { FaCheck } from "react-icons/fa6";
-import { FaUserAlt } from 'react-icons/fa';
-import { MdEmail } from 'react-icons/md';
-import { BsFillTelephoneFill } from 'react-icons/bs';
+import { useSelector } from 'react-redux';
+import { calculateCartTotal } from '@functions/calculateCartTotal';
 
 // components
 import InputForm from '@components/InputForm';
+import CartMessage from '@components/cart/CartMessage';
+import Button from '@components/button/Button';
+
 
 const CartUserProfile = ({ openPaymentModalFunction }) => {
   const { userCartInfo, setUserCartInfo } = useContext(AppContext);
   const { creditCardInfo } = useContext(AppContext);
   const areCCFieldsPopulated = useFieldsPopulated(creditCardInfo);
+  const cart = useSelector((state) => state.cart);
   const isNotDesktopView = useMediaQuery({ query: "(max-width: 992px)" });
 
   const handleSubmit = (values, { setSubmitting }) => {
     setUserCartInfo(values);
-    if (areCCFieldsPopulated) {
+    if (areCCFieldsPopulated && (cart.length > 0)) {
       setTimeout(() => {
         setSubmitting(false);
       }, 3000);
     }
   };
 
+  const parsedTotalCart = parseFloat(calculateCartTotal(cart).replace(/[^\d.-]/g, ''));
+
   return (
     <>
-      <div className="sticky top-0 p-8 rounded-md xl:col-span-2 h-max fading-in-animation">
+      <div className="p-6 bg-white rounded-md shadow xl:col-span-2 fading-in-animation">
         <h2 className="text-2xl">Completa il tuo ordine</h2>
         {
           !isNotDesktopView ?
             <>
               <Suspense fallback={null}>
-                <p className={`${areCCFieldsPopulated ? "visible fade-in" : "invisible"} text-emerald-600 text-md my-2 flex gap-1`}>
-                  Metodo di pagamento aggiunto correttamente <FaCheck className='relative top-1' />
-                </p>
+                <CartMessage
+                  message={'Metodo di pagamento aggiunto correttamente'}
+                  isVisible={areCCFieldsPopulated}
+                />
               </Suspense>
             </> : null
         }
         <Formik
           initialValues={userCartInfo}
-          onSubmit={handleSubmit}
+          onSubmit={(values, { setSubmitting }) => {
+            if (cart.length > 0) {
+              handleSubmit(values, { setSubmitting });
+            } else {
+              setSubmitting(false);
+              alert('nessun item aggiunto');
+            }
+          }}
         >
           {({ isSubmitting }) => (
             <Form className="mt-5 md:mt-10">
               <div>
                 <h3 className="mb-5 text-lg">I tuoi dati personali</h3>
-                <div className="grid gap-4 sm:grid-cols-2">
-                  <div className="flex">
-                    <Field type="text" name="nome" placeholder="Il tuo nome" component={InputForm} />
-                    <FaUserAlt className="relative right-6 top-5" />
-                  </div>
-                  <div className="flex">
-                    <Field type="text" name="cognome" placeholder="Il tuo cognome" component={InputForm} />
-                    <FaUserAlt className="relative right-6 top-5" />
-                  </div>
-                  <div className="flex">
-                    <Field type="email" name="email" placeholder="La tua email" component={InputForm} />
-                    <MdEmail className="relative right-6 top-5" />
-                  </div>
-                  <div className="flex">
-                    <Field type="text" name="telefono" placeholder="Numero di telefono" component={InputForm} />
-                    <BsFillTelephoneFill className="relative right-6 top-5" />
-                  </div>
+                <div className="grid gap-4 gap-x-6 sm:grid-cols-2">
+                  <Field type="text" name="nome" placeholder="Il tuo nome" component={InputForm} />
+                  <Field type="text" name="cognome" placeholder="Il tuo cognome" component={InputForm} />
+                  <Field type="email" name="email" placeholder="La tua email" component={InputForm} />
+                  <Field type="text" name="telefono" placeholder="Numero di telefono" component={InputForm} />
                 </div>
               </div>
               <div className="mt-8">
                 <h3 className="mb-5 text-lg">Indirizzo di spedizione</h3>
-                <div className="grid gap-4 sm:grid-cols-2">
-                  <div className="flex max-w-[424px]">
-                    <Field type="text" name="indirizzo" placeholder="Indirizzo, via/strada .." component={InputForm} />
-                  </div>
-                  <div className="flex max-w-[424px]">
-                    <Field type="text" name="città" placeholder="Città" component={InputForm} />
-                  </div>
-                  <div className="flex max-w-[424px]">
-                    <Field type="text" name="paese" placeholder="Paese" component={InputForm} />
-                  </div>
-                  <div className="flex max-w-[424px]">
-                    <Field type="text" name="zip" placeholder="Zip Code" component={InputForm} />
-                  </div>
+                <div className="grid gap-4 gap-x-6 sm:grid-cols-2">
+                  <Field type="text" name="indirizzo" placeholder="Indirizzo, via/strada .." component={InputForm} />
+                  <Field type="text" name="città" placeholder="Città" component={InputForm} />
+                  <Field type="text" name="paese" placeholder="Paese" component={InputForm} />
+                  <Field type="text" name="zip" placeholder="Zip Code" component={InputForm} />
                 </div>
-                <div className='mt-8'>
-                  <button
+                <div className='mt-8 lg:mt-10'>
+                  <Button
                     type="button"
                     onClick={openPaymentModalFunction}
                     disabled={isSubmitting}
-                    className={`w-full px-6 py-3 text-sm font-semibold text-white rounded-md max-w-52 bg-blue-600 hover:bg-blue-700 ${isSubmitting ? "opacity-60" : 'opacity-100'}`}
+                    className={`w-full py-2.5 text-sm font-semibold text-white rounded-md max-w-52 bg-blue-600 hover:bg-blue-700 ${isSubmitting ? "opacity-60" : 'opacity-100'}`}
                   >
                     Metodo di pagamento
-                  </button>
+                  </Button>
                   {
                     isNotDesktopView ?
                       <>
                         <Suspense fallback={null}>
-                          <p className={`${areCCFieldsPopulated ? "visible fade-in" : "invisible"} text-emerald-600 text-md mt-5 flex gap-1`}>
-                            Metodo di pagamento aggiunto correttamente  <FaCheck className='relative top-1' />
-                          </p>
+                          <CartMessage
+                            message={'Metodo di pagamento aggiunto correttamente'}
+                            isVisible={areCCFieldsPopulated}
+                          />
                         </Suspense>
                       </> : null
                   }
                 </div>
                 <div className="divider my-7 lg:my-10"></div>
-                <div className='flex mt-8 xl:justify-end'>
-                  <button
+                <div className='flex justify-center mt-8 lg:mt-16 md:justify-end'>
+                  <Button
                     type="submit"
                     disabled={isSubmitting || !areCCFieldsPopulated}
-                    className={`w-full max-w-sm px-6 py-3 text-sm font-semibold text-white rounded-md bg-emerald-600 ${areCCFieldsPopulated ? 'opacity-100 hover:bg-emerald-700' : 'opacity-50'}`}
+                    className={`w-full max-w-sm px-6 py-3 text-sm font-semibold text-white rounded-md bg-emerald-600 ${areCCFieldsPopulated && parsedTotalCart > 0 ? 'opacity-100 hover:bg-emerald-700' : 'opacity-50'}`}
                   >
                     {isSubmitting ? 'Inviando...' : 'Conferma ordine'}
-                  </button>
+                  </Button>
                 </div>
               </div>
             </Form>
