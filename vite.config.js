c596d58d2bcfe5564402bbd9518e339d5417ce5f
mattia <sharp798@yahoo.com>
2024-04-30 13:38:19 +0200

    vite.config.js build settings

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,30 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
+// plugins and settings
+import { imageSettings } from './src/settings/imageSettings';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer(imageSettings),
+  ],
   server: {
     watch: {
       usePolling: true,
     },
     port: 3000,
+  },
+  build: {
+    sourcemap: true,
+    minify: 'esbuild',
+    cssCodeSplit: true,
+    cssMinify: "lightningcss" // experimental support for Lightning CSS. Requires 'npm add -D lightningcss'
+  },
+  html: {
+    cspNonce: true, // add a nonce attribute to the <script> tag for CSP (Content Security Policy)
   },
   resolve: {
     alias: {
@@ -25,5 +41,5 @@ export default defineConfig({
       '@languages': path.resolve(__dirname, 'src/languages/'),
       '@settings': path.resolve(__dirname, 'src/settings/'),
     }
-  }
+  },
 });
