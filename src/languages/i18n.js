561d351ed5ae299ba1508305fd697726ae02c4e4
mattiach <sharp798@yahoo.com>
2024-05-03 13:47:13 +0200

    french language

--- src/languages/i18n.js
+++ src/languages/i18n.js
@@ -5,13 +5,14 @@ import { supportedLanguages } from "@settings/supportedLanguages";
 // traslations
 import { en } from "@languages/en/translations";
 import { it } from "@languages/it/translations";
+import { fr } from "@languages/fr/translations";
 
 i18n
   .use(initReactI18next)
   .init({
     // debug: true, 
     fallbackLng: 'en', // language to use if translations in user language are not available
-    resources: { en, it }, // available languages in the application
+    resources: { en, it, fr }, // available languages in the application
     supportedLngs: supportedLanguages,
     interpolation: {
       escapeValue: true // disable XSS (cross site scripting) - attacks
