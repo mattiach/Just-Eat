85867e13e88c284955cd50caa9f4044deafb394e
mattiach <sharp798@yahoo.com>
2024-10-27 16:34:15 +0100

    minor fix

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
   apiVersion: '2024-09-30.acacia',
 });
 
